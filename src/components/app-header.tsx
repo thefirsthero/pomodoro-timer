421a353f69ad45bc8b68e9f8edc0fea64142a25f
thefirsthero <vukosi90@gmail.com>
2025-07-25 15:16:34 +0200

    more header cleanup

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -5,16 +5,13 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { ChevronDown } from "lucide-react";
 import { AppLogo } from "./app-logo";
 import { AppSidebar } from "./app-sidebar";
-import { Button, buttonVariants } from "./ui/button";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import { appConfig, baseUrl } from "@/config/app";
+import { buttonVariants } from "./ui/button";
+import { appConfig } from "@/config/app";
 import GitHub from "./icons/github";
 
 export function AppHeader() {
