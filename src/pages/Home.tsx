d90f9102e6936ebf1bf2dfb10f784765025b0319
thefirsthero <vukosi90@gmail.com>
2025-07-25 23:58:16 +0200

    Home screen cleanup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,13 @@
 import { PageHeader, PageHeaderHeading } from "@/components/page-header";
 import TimerButton from "../components/TimerButton.tsx";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 
 export default function Home() {
   return (
     <>
       <PageHeader>
-        <PageHeaderHeading>Home</PageHeaderHeading>
+        <PageHeaderHeading>Focus Timer</PageHeaderHeading>
       </PageHeader>
-      <Card>
-        <CardHeader>
-          <CardTitle>Focus Timer</CardTitle>
-          <CardDescription>Implementation will go in here.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <TimerButton></TimerButton>
-        </CardContent>
-      </Card>
+      <TimerButton />
     </>
   );
 }
