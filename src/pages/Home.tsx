b33ead2afe1cbe788040b16fee530d8602fb99fd
thefirsthero <vukosi90@gmail.com>
2025-07-27 18:47:37 +0200

    Removed header from homescreen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,10 @@
 import { PageHeader, PageHeaderHeading } from "@/components/page-header";
-import TimerButton from "../components/PomodoroTimer.tsx";
+import PomodoroTimer from "../components/PomodoroTimer.tsx";
 
 export default function Home() {
   return (
     <>
-      <PageHeader>
-        <PageHeaderHeading>Focus Timer</PageHeaderHeading>
-      </PageHeader>
-      <TimerButton />
+      <PomodoroTimer />
     </>
   );
 }
